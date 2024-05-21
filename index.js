f9fc63eb6f5495c3a84eaf61a90fa448be45abb5
Tiago Terenas Almeida <tiagoterenas@gmail.com>
2024-05-21 11:22:22 +0100

    #2 & #4

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
+// @ts-check
+/**
+ * @typedef {Object} DateTimeOptions
+ * @extends Intl.RelativeTimeFormatOptions
+ * @property {string} [locales] - The locale to use for formatting.
+ */
+
+/**
+ * @typedef {Object} RelativeOptions
+ * @extends Intl.RelativeTimeFormatOptions
+ * @property {Date} [from] - The reference date for relative time calculations.
+ * @property {string} [locales] - The locale to use for formatting.
+ */
+
+// @ts-ignore
 const WEEK_IN_MILLIS = 6.048e8;
 const DAY_IN_MILLIS = 8.64e7;
 const HOUR_IN_MILLIS = 3.6e6;
 const MIN_IN_MILLIS = 6e4;
 const SEC_IN_MILLIS = 1e3;
 
+/**
+ * A class representing a date and time.
+ */
 export class DateTime {
   #options = {};
   #instance;
 
+  /**
+   * Returns the earlier of two date and time objects.
+   *
+   * @param {DateTime} a The first date and time object.
+   * @param {DateTime} b The second date and time object.
+   * @returns {DateTime} The date and time with the earliest time value.
+   */
   static min(a, b) {
     if (a instanceof DateTime && b instanceof DateTime)
       return a.time > b.time ? a : b;
     return null;
   }
 
+  /**
+   * Returns the later of two date and time objects.
+   *
+   * @param {DateTime} a The first date and time object.
+   * @param {DateTime} b The second date and time object.
+   * @returns {DateTime} The date and time with the latest time value.
+   */
   static max(a, b) {
     if (a instanceof DateTime && b instanceof DateTime)
       return a.time < b.time ? a : b;
     return null;
   }
 
+  /**
+   * Checks if two date and time objects are equal.
+   *
+   * @param {DateTime} a The first date and time object.
+   * @param {DateTime} b The second date and time object.
+   * @returns {boolean} Whether both date and time objects are equal.
+   */
   static equals(a, b) {
     return a instanceof DateTime && a.equals(b);
   }
 
+  /**
+   * Creates a new DateTime instance.
+   * 
+   * @param {Date|number|string|DateTime} [value] - The date and time value.
+   * @param {DateTimeOptions} [options] - The options for the date and time.
+   */
   constructor(value, options) {
     if (value) {
-      const typeOf = typeof value;
-      if (typeOf === "string" || typeOf === "number")
+      if (typeof value === "string" || typeof value === "number")
         this.#instance = new Date(value);
       else if (value instanceof Date) this.#instance = new Date(value);
       else if (value instanceof DateTime) {
@@ -38,69 +82,135 @@ export class DateTime {
     Object.assign(this.#options, options);
   }
 
+  /**
+   * @returns {number} The year of the date and time.
+   */
   get year() {
     return this.#instance.getFullYear();
   }
+
+  /**
+   * @param {number} value - The new year of the date and time.
+   */
   set year(value) {
     this.#instance.setFullYear(value);
   }
 
+  /**
+   * @returns {number} The month of the date and time.
+   */
   get month() {
     return this.#instance.getMonth();
   }
+
+  /**
+   * @param {number} value - The new month of the date and time.
+   */
   set month(value) {
     this.#instance.setMonth(value);
   }
 
+  /**
+   * @returns {number} The day of the month of the date and time.
+   */
   get day() {
     return this.#instance.getDate();
   }
 
+  /**
+   * @param {number} value - The new day of the month of the date and time.
+   */
   set day(value) {
     this.#instance.setDate(value);
   }
 
+  /**
+   * @returns {number} The hour of the date and time.
+   */
   get hours() {
     return this.#instance.getHours();
   }
+
+  /**
+   * @param {number} value - The new hour of the date and time.
+   */
   set hours(value) {
     this.#instance.setHours(value);
   }
 
+  /**
+   * @returns {number} The minute of the date and time.
+   */
   get minutes() {
     return this.#instance.getMinutes();
   }
+
+  /**
+   * @param {number} value - The new minute of the date and time.
+   */
   set minutes(value) {
     this.#instance.setMinutes(value);
   }
 
+  /**
+   * @returns {Date} A date instance.
+   */
   get date() {
     return new Date(this.#instance);
   }
 
+  /**
+   * @returns {DateTimeOptions} The options for the date and time.
+   */
   get options() {
     return this.#options;
   }
 
+  /**
+   * @returns {number} The time in milliseconds.
+   */
   get time() {
     return this.#instance.getTime();
   }
 
+  /**
+   * Gets the ISO 8601 formatted date and time.
+   *
+   * @returns {string} The ISO 8601 formatted date and time.
+   */
   toISOString() {
     return this.#instance.toISOString();
   }
 
+  /**
+   * Gets the default formatted date and time.
+   *
+   * @param {string} [locales] - The locale to use for formatting.
+   * @returns {string} The formatted date and time.
+   */
   toString(locales) {
     return new Intl.DateTimeFormat(locales || getLang(), this.#options).format(
       this.#instance,
     );
   }
 
+  /**
+   * Gets the formatted date and time.
+   *
+   * @param {DateTimeOptions} [options] - The Intl.DateTimeFormat options object.
+   * @returns {string} The formatted date and time.
+   */
   format(options) {
     const locales = options?.locales || getLang();
     return new Intl.DateTimeFormat(locales, options).format(this.#instance);
   }
 
+  /**
+   * Checks whether the date and time is equal to the target date and time.
+   *
+   * @param {DateTime} target - Comparison target.
+   * @returns {boolean} Whether the date and time is equal to the target date and time.
+   */
   equals(target) {
     const formatter = new Intl.DateTimeFormat(getLang(), this.#options);
     return (
@@ -109,10 +219,23 @@ export class DateTime {
     );
   }
 
+  /**
+   * Creates a clone of the current date and time object.
+   *
+   * @returns {DateTime} A new date and time object that is a clone of the current object.
+   */
   clone() {
     return new DateTime(this.#instance, this.#options);
   }
 
+  /**
+   * Calculates the difference between two date and times.
+   *
+   * @param {DateTime} target - The target date and time.
+   * @param {string} output - The output format.
+   * @param {boolean} [float] - Whether to use floating-point numbers.
+   * @returns {number} The difference between the two date and times.
+   */
   diff(target, output, float) {
     if (!(target instanceof DateTime)) return null;
     const format = (input) => (float ? input : Math.floor(input));
@@ -129,6 +252,12 @@ export class DateTime {
     return format(result);
   }
 
+  /**
+   * Gets the relative time from the reference date.
+   *
+   * @param {RelativeOptions} options - The options for relative time calculations.
+   * @returns {string} The relative time from the reference date.
+   */
   relative(options) {
     const from = options?.from || new Date();
     const locales = options?.locales || getLang();
@@ -160,7 +289,22 @@ export class DateTime {
   }
 }
 
+/**
+ * Creates a new date and time object with default date format.
+ *
+ * @param {Date|number|string|DateTime} [value] - The date and time value.
+ * @param {DateTimeOptions} [options] - The options for the date and time.
+ * @returns {DateTime} A new date and time object.
+ */
 export const date = (value, options) => new DateTime(value, options);
+
+/**
+ * Creates a new date and time object with default time format.
+ *
+ * @param {Date|number|string|DateTime} [value] - The date and time value.
+ * @param {DateTimeOptions} [options] - The options for the date and time.
+ * @returns {DateTime} A new date and time object.
+ */
 export const time = (value, options) =>
   new DateTime(
     value,
